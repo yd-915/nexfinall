8855c721456410c35e6488d81ba1af8c5943f48b
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2022-09-01 19:31:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -50,10 +50,10 @@ const ChatRoom = require('./server/models/ChatRoom')
 });
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('/client/build'));
+    app.use(express.static(__dirname, 'client', 'build'));
     
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '/client/build/index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 } else {
     app.get("/*", (req, res) => {
