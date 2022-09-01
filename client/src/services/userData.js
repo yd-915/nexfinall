bbe4e11aa337b91ff19b70475296cf4b4cad7409
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2022-09-01 18:32:40 -0400

    Update userData.js

--- client/src/services/userData.js
+++ client/src/services/userData.js
@@ -1,4 +1,4 @@
-const baseUrl = 'https://nexnotesapp.herokuapp.com/';
+const baseUrl = 'https://notexchange.shop/';
 
 export async function registerUser(userData) {
     return (await fetch(`${baseUrl}/auth/register`, {
