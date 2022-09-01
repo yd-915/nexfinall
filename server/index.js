31df3bb5e29a18a9564270511e0dcb77aad24f9e
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2022-09-01 16:41:50 -0400

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const { PORT } = require('./config/config');
+const auth = require('./middlewares/auth')
+const routes = require('./routes');
+const http = require('http').createServer(app);
+require("dotenv").config();
+require('./config/express')(app);
+require('./config/mongoose');
+app.use(auth())
+
+const io = require("socket.io")(http, {
+    cors: {
+        origin: ["https://nexnotes.shop/"],
+       credentials: true
+    }
+ });
+ const ChatRoom = require('./models/ChatRoom')
+ 
+    io.on('connection', async function (socket) {
+    
+   
+   let asd = await ChatRoom.find().populate("buyer").populate("seller");
+ 
+  socket.emit('output', asd);
+ 
+  socket.on('input', function(data){
+     console.log(data)
+ });
+ });
+ 
+ 
+   io.on('input', async function (data) {
+    console.log(data)
+ 
+     let chat = await ChatRoom.findById(chatId);
+    console.log(chat)
+    chat.insert({name: name, message: message}, function(){
+        io.emit('output', [data]);
+ 
+      
+       sendStatus({
+           message: 'Message sent',
+           clear: true
+      });
+    });
+ 
+ });
+
+
+app.use(routes);
+app.listen(PORT, () => console.log(`Server is active at https://nexnotesapp.herokuapp.com/${PORT}...`));
+// app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
